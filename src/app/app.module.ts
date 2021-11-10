81ed454d2d4a24692cc09b855f61b1d9e2a60f9d
Yasmine99college <yasminaahmedabcd@gmail.com>
2021-11-10 03:38:53 +0200

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,8 @@ const appRoutes:Routes
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
